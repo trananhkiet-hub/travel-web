151bebb62ce4712aa5bd2de69d50537375767994
trananhkiet <2451052133kiet@ou.edu.vn>
2025-08-14 16:19:00 +0700

    ADD HTML CSS JS

--- web.js
+++ web.js
@@ -1,21 +1,4 @@
-
-        function togglePassword() {
-            const passwordInput = document.getElementById('password');
-            const eyeIcon = document.querySelector('.eye-icon');
-            const eyeOffIcon = document.querySelector('.eye-off-icon');
-            
-            if (passwordInput.type === 'password') {
-                passwordInput.type = 'text';
-                eyeIcon.style.display = 'none';
-                eyeOffIcon.style.display = 'block';
-            } else {
-                passwordInput.type = 'password';
-                eyeIcon.style.display = 'block';
-                eyeOffIcon.style.display = 'none';
-            }
-        }
-
-        function handleLogin(event) {
+  function handleLogin(event) {
             event.preventDefault();
             const email = document.getElementById('email').value;
             const password = document.getElementById('password').value;
